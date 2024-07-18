6df08016f587933204064f4e9969510fc0aaec67
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2024-07-18 12:48:42 +0530

    Fix bug in ci pipeline

--- utils/Authentication.ts
+++ utils/Authentication.ts
@@ -5,7 +5,7 @@ import {
   onAuthStateChanged,
   User as FirebaseUser,
 } from "firebase/auth";
-import { auth } from "@/config/firebase";
+import { auth } from "../config/firebase";
 
 export type User = {
   email: string;
